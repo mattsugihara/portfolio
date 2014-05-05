a98b4f5957a533532c6961d13f5f270dd7dcbb55
Matt Sugihara <msugihara@fluid.com>
2014-05-04 23:58:00 -0700

    created a watch for oakley, arranged source assets

---
+++ js/ticker.js
@@ -0,0 +1,54 @@
+var hour;
+var minute;
+var second;
+
+function getCurrentTime () {
+	var date = new Date ();
+	
+	hour = date.getHours();
+	minute = date.getMinutes();
+	second = date.getSeconds();
+}
+
+function spinHands () {
+
+	hourHandPosition = (hour%12)*30;
+	minuteHandPosition = minute*6;
+	secondHandPosition = second*6;
+
+	var hourHand = document.getElementById('hour');
+	var minuteHand = document.getElementById('minute');
+	var secondHand = document.getElementById('second');
+
+	hourHand.style.webkitTransform = 'rotate('+hourHandPosition+'deg)';
+  hourHand.style.mozTransform    = 'rotate('+hourHandPosition+'deg)';
+  hourHand.style.msTransform     = 'rotate('+hourHandPosition+'deg)';
+  hourHand.style.oTransform      = 'rotate('+hourHandPosition+'deg)';
+  hourHand.style.transform       = 'rotate('+hourHandPosition+'deg)';
+
+  minuteHand.style.webkitTransform = 'rotate('+minuteHandPosition+'deg)';
+  minuteHand.style.mozTransform    = 'rotate('+minuteHandPosition+'deg)';
+  minuteHand.style.msTransform     = 'rotate('+minuteHandPosition+'deg)';
+  minuteHand.style.oTransform      = 'rotate('+minuteHandPosition+'deg)';
+  minuteHand.style.transform       = 'rotate('+minuteHandPosition+'deg)';
+
+  secondHand.style.webkitTransform = 'rotate('+secondHandPosition+'deg)';
+  secondHand.style.mozTransform    = 'rotate('+secondHandPosition+'deg)';
+  secondHand.style.msTransform     = 'rotate('+secondHandPosition+'deg)';
+  secondHand.style.oTransform      = 'rotate('+secondHandPosition+'deg)';
+  secondHand.style.transform       = 'rotate('+secondHandPosition+'deg)';
+}
+
+function updateClock () {
+	getCurrentTime();
+	spinHands();
+	
+	// set digital clock, remove
+	var dateString = hour + ':' + minute + ':' + second;
+	var clock = document.getElementById('clock');
+	clock.innerHTML = dateString;
+
+}
+
+updateClock();
+setInterval('updateClock()', 500);
