6b314433c3188aeb747cd7e27bf67db25ac5f2f7
Matt Sugihara <msugihara@fluid.com>
2014-06-10 00:01:35 -0700

    optimizations

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "javascripts"
+output_style = :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -14,7 +15,7 @@ javascripts_dir = "javascripts"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
